aaf7d624e068b08309e49fad037f9b54a5489344
Ankit Gujare <80085008+anky19698@users.noreply.github.com>
2024-08-08 14:39:29 +0530

    Update langchain_rag.py

--- langchain_rag.py
+++ langchain_rag.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from langchain.vectorstores import Chroma
 import os
 from langchain_huggingface import HuggingFaceEmbeddings, HuggingFaceEndpoint
